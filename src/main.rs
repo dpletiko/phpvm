bfe468f62da30048af3558a9950a7e9cabc046a1
Dino Pletikosa <dino.pletikosa@exevio.com>
2024-05-07 20:51:45 +0200

    Basic debian use version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::{Error, Result};
 use clap::{Parser, Subcommand};
 use serde_json::Value;
-use std::{fs::File, io::BufReader, process::{Command, Stdio}};
+use sys_info::LinuxOSReleaseInfo;
+use std::{env::{self, consts}, fs::File, io::BufReader, process::{Command, Stdio}};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -120,7 +121,7 @@ fn use_composer() -> Result<(), Error> {
 
     let file = match File::open("composer.json") {
         Ok(file) => file,
-        Err(err) => return Err(Error::msg(format!("Problem opening the file: {:?}", err)))
+        Err(_) => return Err(Error::msg(format!("Composer.json not found at current location [{:?}]", env::current_dir()?.display())))
     };
 
     let reader = BufReader::new(file);
@@ -165,5 +166,77 @@ fn use_composer() -> Result<(), Error> {
 fn use_version(version: String) -> Result<(), Error> {
     println!("Using version: {}", version);
 
+    match consts::OS {
+        "linux" => match sys_info::linux_os_release()? {
+            LinuxOSReleaseInfo { id, .. } if id == Some("debian".to_owned()) => debian(version),
+            LinuxOSReleaseInfo { id, .. } => Err(Error::msg(format!("Distro [{:?}] not yet handled!", id))),
+        },
+        "windows" => windows(version),
+        s => Err(Error::msg(s.to_string())),
+    }
+}
+
+fn debian(version: String) -> Result<(), Error> {
+    println!("You on Linux Debian, bro");
+
+    println!("/usr/bin/php{}", version);
+
+    let php = Command::new("update-alternatives")
+        .args(&["--set", "php"])
+        .arg(format!("/usr/bin/php{}", version))
+        .output();
+
+    match php {
+        Ok(o) => println!("{:?}", String::from_utf8_lossy(&o.stdout)),
+        Err(err) => return Err(Error::from(err))
+    }
+
+    let phar = Command::new("update-alternatives")
+        .args(&["--set", "phar"])
+        .arg(format!("/usr/bin/phah{}", version))
+        .output();
+
+    match phar {
+        Ok(o) => println!("{:?}", String::from_utf8_lossy(&o.stdout)),
+        Err(err) => return Err(Error::from(err))
+    }
+
+    let phar_phar = Command::new("update-alternatives")
+        .args(&["--set", "phar.phar"])
+        .arg(format!("/usr/bin/phar.phar{}", version))
+        .output();
+
+    match phar_phar {
+        Ok(o) => println!("{:?}", String::from_utf8_lossy(&o.stdout)),
+        Err(err) => return Err(Error::from(err))
+    }
+
+    let phpize = Command::new("update-alternatives")
+        .args(&["--set", "phpize"])
+        .arg(format!("/usr/bin/phpize{}", version))
+        .output();
+
+    match phpize {
+        Ok(o) => println!("{:?}", String::from_utf8_lossy(&o.stdout)),
+        Err(err) => return Err(Error::from(err))
+    }
+
+    let php_config = Command::new("update-alternatives")
+        .args(&["--set", "php-config"])
+        .arg(format!("/usr/bin/php-config{}", version))
+        .output();
+
+    match php_config {
+        Ok(o) => println!("{:?}", String::from_utf8_lossy(&o.stdout)),
+        Err(err) => return Err(Error::from(err))
+    }
+
     Ok(())
+}
+
+
+fn windows(version: String) -> Result<(), Error> {
+    println!("You on Windows, bro");
+
+    Err(Error::msg("Exitting!"))
 }
